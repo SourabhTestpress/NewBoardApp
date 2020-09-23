1a3b36f889ce2444c63e6825668bfab1abd66e04
Sourabh <sourabh@testpress.in>
2020-09-23 15:41:17 +0530

    Improved Models.py file and added pre commit hooks setup

--- boards/models.py
+++ boards/models.py
@@ -1,54 +1,44 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.utils.text import slugify
+from django.conf import settings
 from django.urls import reverse
+from model_utils.models import TimeStampedModel
 
-User = get_user_model()
+User = settings.AUTH_USER_MODEL
 
 
 class Board(models.Model):
-	name = models.CharField(max_length = 100,unique = True)
-	slug = models.SlugField(blank=True)
-	description = models.TextField(max_length = 500)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=500)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def save(self,*args,**kwargs):
-		if not self.id:
-			self.slug = slugify(self.name)
-
-		super(Board,self).save(*args,**kwargs)
-
-	def get_absolute_url(self):
-		return '/board/{}/'.format(self.slug)
-
+    def get_absolute_url(self):
+        return reverse("board_details", args=(self.slug,))
 
 
 class Topic(models.Model):
-	board = models.ForeignKey(Board,on_delete = models.CASCADE,related_name = 'topics')
-	slug = models.SlugField(blank=True)
-	user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'topics')
-	subject = models.CharField(max_length=100)
-	last_updated = models.DateTimeField(auto_now_add = True)
+    board = models.ForeignKey("Board", on_delete=models.CASCADE, related_name="topics")
+    slug = models.SlugField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
+    subject = models.CharField(max_length=100)
+    last_updated = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.subject
+    def __str__(self):
+        return self.subject
 
-	def save(self,*args,**kwargs):
-		if not self.id:
-			self.slug = slugify(self.subject)
-
-		super(Topic,self).save(*args,**kwargs)
+    def get_absolute_url(self):
+        return reverse("topic_details", args=(self.slug,))
 
 
 class Post(models.Model):
-	topic = models.ForeignKey(Topic,on_delete = models.CASCADE,related_name = 'posts')
-	created_by = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'posts')
-	updated_by = models.ForeignKey(User,on_delete = models.CASCADE,null = True,related_name = '+')
-	message = models.TextField(max_length=2000)
-	created_at = models.DateTimeField(auto_now_add = True)
-	updated_at = models.DateTimeField(null = True)
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE, related_name="posts")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="+"
+    )
+    message = models.TextField(max_length=2000)
 
-	def __str__(self):
-		return str(self.message)[:20]
+    def __str__(self):
+        return str(self.message)[:20]

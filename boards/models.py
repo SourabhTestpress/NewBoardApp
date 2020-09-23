1c8f904019765e31ae34d01d769c02ad7d9e2e0f
Sourabh <sourabh@testpress.in>
2020-09-23 18:04:34 +0530

    Models.py file added with changes suggested

---
+++ boards/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from model_utils.models import TimeStampedModel
+
+User = settings.AUTH_USER_MODEL
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("board_details", args=(self.slug,))
+
+
+class Topic(models.Model):
+    board = models.ForeignKey("Board", on_delete=models.CASCADE, related_name="topics")
+    slug = models.SlugField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
+    subject = models.CharField(max_length=100)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+    def get_absolute_url(self):
+        return reverse("topic_details", args=(self.slug,))
+
+
+class Post(models.Model):
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE, related_name="posts")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="+"
+    )
+    message = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return str(self.message)[:20]

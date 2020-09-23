d183fb89ab7e36f20fe27c614430865710ec4112
Sourabh <sourabh@testpress.in>
2020-09-23 17:09:10 +0530

    removed views,tests and templates

--- boards/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.board_list, name="home"),
-    path("board/<str:slug>/", views.board_details, name="board_details"),
-]

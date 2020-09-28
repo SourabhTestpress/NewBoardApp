c7f96282c601394462c7f4a1b6f7034811edfb21
Sourabh <sourabh@testpress.in>
2020-09-28 11:27:08 +0530

    Settings edited

--- BoardProject/settings.py
+++ BoardProject/settings.py
@@ -81,6 +81,11 @@ DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
+DATABASES["default"]["CONN_MAX_AGE"] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

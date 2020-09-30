e2df87c426b5dd88ce1c495cd689f3270ef1b8bd
Sourabh <sourabh@testpress.in>
2020-09-30 17:08:39 +0530

    Add Test to verify view of BoardDetailView

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.urls import resolve, reverse
 from django.test import SimpleTestCase, TestCase, RequestFactory
 
-from boards.views import board_list, topic_list
+from boards.views import board_list, topic_list, board_detail
+from boards.models import Board
 
 
 class BoardListViewTests(SimpleTestCase):
@@ -37,4 +38,22 @@ class TopicListViewTestsInvolvingDatabase(TestCase):
     def test_topic_list_view_produces_status_code_200(self):
         request = self.factory.get(reverse("all_topics"))
         response = board_list(request)
+        self.assertEquals(200, response.status_code)
+
+
+class BoardDetailView(TestCase):
+    """ It tests the views of BoardDetailView"""
+
+    def setUp(self):
+        self.board_obj = Board.objects.create(
+            name="1st", slug="slug-of-board1", description="board_obj"
+        )
+        self.factory = RequestFactory()
+
+    def test_board_detail_is_refrencing_to_correct_associated_function(self):
+        view = resolve("/slug_of_board1/")
+        self.assertEquals(view.func, board_detail)
+
+    def test_board_detail_view_produces_status_code_200(self):
+        response = self.client.get(reverse("detail", args=(self.board_obj.slug,)))
         self.assertEquals(200, response.status_code)
